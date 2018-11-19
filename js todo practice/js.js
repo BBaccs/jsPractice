09dbf39bc07c418c6c4b613629dfdcc9d958c475
Brian Baccarella <baccarellab@gmail.com>
2018-11-19 15:29:47 -0500

    add random colors

--- js todo practice/js.js
+++ js todo practice/js.js
@@ -2,10 +2,14 @@
 let lis = document.querySelectorAll("li");
 
 //loop through li's
-for(var i = 0; i <lis.length; i++){
+for(var i = 0; i < lis.length; i++){
     lis[i].addEventListener("click", function(){
         this.classList.toggle("done");
     });
 }
 
 
+
+for(var i = 0; i < codes.length; i++) {
+    codes[i].print("here's a code!");
+}
